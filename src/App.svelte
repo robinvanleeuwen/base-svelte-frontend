f65cfccf727968d4679053a8513c41e21afa1208
Robin van Leeuwen <robinvanleeuwen@gmail.com>
2022-02-20 12:06:52 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import ForgotPassword from './routes/ForgotPassword.svelte';
+	import SignIn from './routes/SignIn.svelte';
+	import SignUp from './routes/SignUp.svelte';
+	import Router from 'svelte-spa-router';
+	
+	const routes = 	{
+		"/": SignUp,
+		"/signin": SignIn,
+		"/forgotpassword": ForgotPassword
+	};
+
+</script>
+
+<main>
+	<Router {routes}/>	
+</main>
+
---
+++ src/routes/SignIn.svelte
@@ -0,0 +1,3 @@
+<div class="text-center">
+    Hello
+</div>
